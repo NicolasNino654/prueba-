44f292a564af6a2398c8692ae4d40fd5210e7025
Estudiantes <Estudiantes@labing50420.udistrital.edu.co>
2017-08-11 16:13:07 -0500

    Impresion de "Hola mundo" utilizando la herramienta de imprimir

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -10,12 +10,10 @@ package prueba;
  * @author Estudiantes
  */
 public class Prueba {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+        System.out.println("Hola mundo"); 
+                }
     
 }
